e8e52a1b0ad8745bf13f01ee7a089b1e7da0e5c3
Mal Curtis <mal@mal.co.nz>
2014-08-17 16:55:18 +1200

    Update event struct

--- event.go
+++ event.go
@@ -3,7 +3,9 @@ package webhooks
 import "encoding/json"
 
 type Event struct {
+	Id        string          `json:"id"`
+	Message   string          `json:"message"`
 	Type      string          `json:"type"`
-	Timestamp string          `json:"timestamp"`
+	Timestamp int64           `json:"timestamp"`
 	Data      json.RawMessage `json:"data"`
 }
